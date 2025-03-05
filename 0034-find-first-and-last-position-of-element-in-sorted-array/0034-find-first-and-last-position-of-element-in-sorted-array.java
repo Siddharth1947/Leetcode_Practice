8eebec610ac557b293bbff92fcfd1bc11ac12f82
Siddharth Singh <119068884+Siddharth1947@users.noreply.github.com>
2025-03-05 14:47:43 +0530

    Time: 1 ms (20.53%), Space: 46.4 MB (8.76%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,25 @@
 class Solution {
-    public int[] searchRange(int[] arr, int target) {
-        int[] result = {-1, -1};  
-        result[0] = findFirst(arr, target);
-        result[1] = findLast(arr, target);
+    public int[] searchRange(int[] nums, int target) {
+        int [] result={-1,-1};
+        result[0]=first(nums,target);
+        result[1]=last(nums,target); 
         return result;
     }
+    private int first(int[] nums,int target){
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    private int last(int[] nums,int target){
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
     
-    private int findFirst(int[] arr, int target) {
-        int start = 0, end = arr.length - 1;
-        int first = -1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (arr[mid] == target) {
-                first = mid;
-                end = mid - 1; 
-            } else if (arr[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return first;
-    }
-
-    private int findLast(int[] arr, int target) {
-        int start = 0, end = arr.length - 1;
-        int last = -1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (arr[mid] == target) {
-                last = mid;
-                start = mid + 1; 
-            } else if (arr[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return last;
-    }
 }
