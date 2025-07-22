3d88701228302d30f2c66b781022755a27477b03
Siddharth Singh <119068884+Siddharth1947@users.noreply.github.com>
2025-07-22 10:23:14 +0530

    Time: 0 ms (100%), Space: 45.8 MB (12.88%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -7,17 +7,26 @@ class Solution {
         //     sum += nums[i];
         // }
         // return totalSum - sum;
-        Set<Integer> result = new HashSet<>();
-        for(int num:nums){
-            result.add(num);
-        }
-        for(int i=0;i<=result.size();i++){
-            if(!result.contains(i)){
-                return i;
+    //     Set<Integer> result = new HashSet<>();
+    //     for(int num:nums){
+    //         result.add(num);
+    //     }
+    //     for(int i=0;i<=result.size();i++){
+    //         if(!result.contains(i)){
+    //             return i;
 
-            }
-        }
-        return -1;
+    //         }
+    //     }
+    //     return -1;
 
+    // 
+    
+    int result = nums.length;  
+    for (int i = 0; i < nums.length; i++) {
+        result ^= i ^ nums[i];
     }
+    return result;
+}
+
+
 }
